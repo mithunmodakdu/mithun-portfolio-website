ab85f2abd9915ddc5484d8babaa064de5a47b2fc
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-02-24 01:47:04 +0600

    Banner designation with type effect part has been completed

---
+++ js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+  strings: ["Web Developer", "Full Stack Web Developer", "Frontend Web Developer", "Wordpress Web Developer", "MERN Stack Web Developer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true
+});
